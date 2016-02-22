25162b124b2a21d1a821f1204ef32d0569ccdf42
jon nardine and torrence stratton <jwnardine@gmail.com>
2016-02-22 14:25:17 -0800

    insert twig into results.html.twig so item is properly called and displayed

--- app/app.php
+++ app/app.php
@@ -2,12 +2,28 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Inventory.php";
 
-    session_start();
+    $server = 'mysql:host=localhost;dbname=inventory';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
 
     $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'=>__DIR__."/../views"
     ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('form.html.twig', array('items' => Inventory::getAll()));
+    });
+
+    $app->post("/results", function() use ($app) {
+        $my_Inventory = new Inventory($_POST['items']);
+        $my_Inventory->save();
+        var_dump($my_Inventory);
+        return $app['twig']->render('results.html.twig', array('items' => Inventory::getAll()));
+    });
+
+
 
 
 
